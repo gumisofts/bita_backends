2521dee13f46e92867a52d6310c2a96c7db77e14
nurad <nuradhussen082@gmail.com>
2024-09-06 14:30:36 +0300

    templates dirs fix

--- lib/utils/email/email_templates.dart
+++ lib/utils/email/email_templates.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 class EmailOtpTemplate {
   static Future<String> get content async {
-    final temp = File('lib/utils/email/templates/otp_sign_up_mail.html');
+    final temp = File('templates/otp_sign_up_mail.html');
 
     return temp.readAsString();
   }
